7d27df140dc43d83e43db29b9034abfa0e84b5f6
Daróczi Levente <daroczilevente2@gmail.com>
2025-11-12 17:37:09 +0200

    update diagrams

--- src/zoo/VirtualZoo.java
+++ src/zoo/VirtualZoo.java
@@ -27,30 +27,4 @@ public class VirtualZoo implements Aggregate<Animal> {
     public Iterator<Animal> createIterator() {
         return new AnimalIterator(animals);
     }
-
-    /*public static void main(String[] args) {
-        List<Animal> animals = new ArrayList<>();
-        animals.add(new Mammal("Simba", 4, 50, "HAPPY", 100, "Cat", "lion"));
-        animals.add(new Bird("Polly", 2, 30, "EXCITED", 90, "Parrot", "macaw"));
-        animals.add(new Reptile("Rango", 3, 40, "CALM", 80, "Lizard", "iguana"));
-
-
-        zoo.VirtualZoo zoo = new zoo.VirtualZoo(animals);
-        Iterator<Animal> iterator = zoo.createIterator();
-
-        Visitor feedingVisitor = new visitor.concretevisitor.FeedingVisitor();
-        Visitor medicalVisitor = new visitor.concretevisitor.MedicalVisitor();
-        Visitor reportVisitor = new visitor.concretevisitor.ReportVisitor();
-
-        while (iterator.hasNext()) {
-            Animal animal = iterator.next();
-            System.out.println("--- Visiting " + animal.getName() + " ---");
-            System.out.println(animal);
-            animal.accept(feedingVisitor);
-            animal.accept(medicalVisitor);
-            animal.accept(reportVisitor);
-            System.out.println(animal);
-            System.out.println("------------------------------\n");
-        }
-    }*/
 }
